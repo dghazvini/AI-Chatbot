e13c290c9d1ed7a41bf0fd555ce4cfce778f1d4a
Dan <danielghazvini@gmail.com>
2018-11-25 10:01:32 -0600

    Generic responses testing

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,27 @@
-GREETINGS = ("hi", "hello")
+import random
 
-def checkGreetings(input):
-    a = 3
+GREETINGS_INPUT = ("hi", "hello", "hey")
+GREETINGS_OUTPUT = ("Hello there!, Hi!")
+FAREWELLS_INPUT = ("quit, bye, goodbye, exit, terminate")
+FAREWELLS_OUTPUT = ("See ya!, Goodbye!, Sayonara!")
 
+# Generic greetings
+def checkGreetings(userInput):
+    for word in userInput.words:
+        if word.lower() in GREETINGS_INPUT:
+            print("HERE")
+            print(random.choice(GREETINGS_OUTPUT))
+
+# Quit program if user specifices
+def checkFarewells(userInput):
+    for word in userInput:
+        if word.lower() in FAREWELLS_INPUT:
+            print(random.choice(FAREWELLS_OUTPUT))
+
+# Main method
 def main():
-    userinput = input("Hello there")
-    type(userinput)
-    checkGreetings(userinput)
+    userInput = input("Hello there\n")
+  #  checkGreetings(userInput)
+  #  checkFarewells(userInput)
+
+main()
