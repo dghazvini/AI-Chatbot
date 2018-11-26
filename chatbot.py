3e1779d2e79d060e3b5afcfc946dcabb039a4a54
Dan <danielghazvini@gmail.com>
2018-11-25 20:48:53 -0600

    Test

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,7 @@
 import random
-#import tensorflow as tf
-#import numpy as np
 import re
 import sys
+import numpy
 
 GREETINGS_INPUT = ("hi", "hello", "hey")
 GREETINGS_OUTPUT = ["Hello there!", "Hi!", "What's up?", "Greetings!"]
@@ -18,81 +17,58 @@ def checkGreetings(userInput):
 
 # Quit program if user specifices
 def checkFarewells(userInput):
-    for word in userInput.split():
-        if word.lower() in FAREWELLS_INPUT:
-            print(random.choice(FAREWELLS_OUTPUT))
-            sys.exit()
+    if userInput.lower() in FAREWELLS_INPUT:
+        print(random.choice(FAREWELLS_OUTPUT))
+        sys.exit()
 
-#def train():
+# Neural Net
+def NN(m1, m2, w1, w2, b):
+    z = m1 * w1 + m2 * w2 + b
+    return sigmoid(z)
 
+# Sigmoid function
+def sigmoid(x):
+    return 1/(1 + numpy.exp(-x))
 
-# Main method
-def main():
-    # Load data
-    lines = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
-    conv_lines = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+def giveResponse(userInput):
+ # Generate random weights and bias
+    w1 = numpy.random.randn()
+    w2 = numpy.random.randn()
+    b = numpy.random.randn()
 
-    # The sentences that we will be using to train our model.
-    lines[:10]
+    # Determine what predefined input is most similar to the user's input
+    index=0
+    indexofMaxMatchingWordsQuestion=0
+    matchingWords=0
+    maxMatchingWords=0
+    for question in questions:
+        for word in userInput.split():
+            if word.lower() in question.lower():
+                matchingWords=matchingWords+1
+                if matchingWords > maxMatchingWords:
+                    maxMatchingWords = matchingWords
+                    indexofMaxMatchingWordsQuestion = index
+        index=index+1
+        matchingWords=0
 
-    # The sentences' ids, which will be processed to become our input and target data.
-    conv_lines[:10]
+    # Case for when no words match a question query
+    if maxMatchingWords == 0:
+        index=0
+        for question in questions:
+            for letter in userInput:
+                if letter.lower() in question.lower():
+                    matchingWords=matchingWords+1
+                    if matchingWords > maxMatchingWords:
+                        maxMatchingWords = matchingWords
+                        indexofMaxMatchingWordsQuestion = index
+            index=index+1
+            matchingWords=0
+        print(answers[indexofMaxMatchingWordsQuestion])
+    else:
+        print(answers[indexofMaxMatchingWordsQuestion])
 
-    # Create a dictionary to map each line's id with its text
-    id2line = {}
-    for line in lines:
-        _line = line.split(' +++$+++ ')
-        if len(_line) == 5:
-            id2line[_line[0]] = _line[4]
-
-   # Create a list of all of the conversations' lines' ids.
-    convs = [ ]
-    for line in conv_lines[:-1]:
-        _line = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
-        convs.append(_line.split(',')) 
-
-    # Sort the sentences into questions (inputs) and answers (targets)
-    questions = []
-    answers = []
-
-    for conv in convs:
-        for i in range(len(conv)-1):
-            questions.append(id2line[conv[i]])
-            answers.append(id2line[conv[i+1]])
-
-    # Check if we have loaded the data correctly
-    limit = 0
-    for i in range(limit, limit+5):
-        print(questions[i])
-        print(answers[i])
-        print()
-    
-    # Compare lengths of questions and answers
-    print(len(questions))
-    print(len(answers))
-
-    # Clean the data
-    # clean_questions = []
-    # for question in questions:
-    #     clean_questions.append(clean_text(question))
-    
-    # clean_answers = []    
-    # for answer in answers:
-    #     clean_answers.append(clean_text(answer))
-    
-    # Take a look at some of the data to ensure that it has been cleaned well.
-    # limit = 0
-    # for i in range(limit, limit+5):
-    #     print(clean_questions[i])
-    #     print(clean_answers[i])
-    #     print()
-
-    userInput = input("Hello there, what can I do?\n")
-
-    while True:
-        checkGreetings(userInput)
-        checkFarewells(userInput)
-        userInput = input()
+    print("Activation is ", end=" ")
+    print(NN(3, 1.5, w1, w2, b))
 
 def clean_text(text):
     '''Clean text by removing unnecessary characters and altering the format of words.'''
@@ -122,4 +98,56 @@ def clean_text(text):
     
     return text
 
-main()
+# Load data
+lines = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conv_lines = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# The sentences that we will be using to train our model.
+lines[:10]
+
+# The sentences' ids, which will be processed to become our input and target data.
+conv_lines[:10]
+
+# Create a dictionary to map each line's id with its text
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
+# Create a list of all of the conversations' lines' ids.
+convs = [ ]
+for line in conv_lines[:-1]:
+    _line = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    convs.append(_line.split(',')) 
+
+# Sort the sentences into questions (inputs) and answers (targets)
+questions = []
+answers = []
+
+for conv in convs:
+    for i in range(len(conv)-1):
+        questions.append(id2line[conv[i]])
+        answers.append(id2line[conv[i+1]])
+
+# Check if we have loaded the data correctly
+# limit = 0
+# for i in range(limit, limit+5):
+#     print(questions[i])
+#     print(answers[i])
+#     print()
+
+# Take a look at some of the data to ensure that it has been cleaned well.
+# limit = 0
+# for i in range(limit, limit+5):
+#     print(clean_questions[i])
+#     print(clean_answers[i])
+#     print()
+
+userInput = input("Hello there, what can I do?\n")
+
+while True:
+    checkGreetings(userInput)
+    checkFarewells(userInput)
+    giveResponse(userInput)
+    userInput = input()
